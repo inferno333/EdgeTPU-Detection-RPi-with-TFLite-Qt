34d2c4f39fab7eefb4edfd04fe8e35563859d41b
inferno333 <inferno1968@proton.me>
2021-06-02 12:15:17 +0000

    Add copyright header

--- cpp/edgetpu.h
+++ cpp/edgetpu.h
@@ -3,4 +3,21 @@ Copyright 2018 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
-You may obtain a copy of the License a
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+//
+// This header file defines EdgeTpuManager, and EdgeTpuContext.
+// EdgeTpuContext is an object associated with one or more tflite::Interpreter.
+// Instances of this class should be allocated through
+// EdgeTpuManager::NewEdgeTpuContext.
+// More than one Interpreter instances can point to the same context. This means
+// the tasks from both would be executed under the same TPU context.
+// The lifetime of th
