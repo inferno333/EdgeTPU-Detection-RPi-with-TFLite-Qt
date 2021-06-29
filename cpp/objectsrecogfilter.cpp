60079254a75e129669ac4a2701b7f8acbe9382a3
inferno333 <inferno1968@proton.me>
2021-06-29 16:07:43 +0000

    propellor spin

--- cpp/objectsrecogfilter.cpp
+++ cpp/objectsrecogfilter.cpp
@@ -2,3 +2,22 @@
 #include <QStandardPaths>
 #include <QPainter>
 #include <QDebug>
+#include <QThread>
+#include <QMutexLocker>
+
+#include "auxutils.h"
+#include "private/qvideoframe_p.h"
+
+// WARNING: same TensorFlow initialization repeated in ObjectRecogFilter and TensorFlowQML constructors
+ObjectsRecogFilter::ObjectsRecogFilter()
+{
+    connect(this, SIGNAL(runTensorFlow(QImage)), this, SLOT(TensorFlowExecution(QImage)));
+    connect(&tft,SIGNAL(results(int, QStringList, QList<double>, QList<QRectF>, QList<QImage>, int)),this,SLOT(processResults(int, QStringList, QList<double>, QList<QRectF>, QList<QImage>, int)));
+
+    tf.setFilename(AuxUtils::getDefaultModelFilename());
+    tf.setLabelsFilename(AuxUtils::getDefaultLabelsFilename());
+    tf.setAccelaration(true);
+    tf.setNumThreads(QThread::idealThreadCount());
+
+    releaseRunning();
+    initialized = f
