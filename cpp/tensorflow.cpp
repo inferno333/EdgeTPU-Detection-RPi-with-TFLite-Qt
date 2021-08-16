92a26d42c121a652798733a1afe69eee3abbabc3
inferno333 <inferno1968@proton.me>
2021-08-16 05:50:31 +0000

    add deprecation warning

--- cpp/tensorflow.cpp
+++ cpp/tensorflow.cpp
@@ -12,4 +12,31 @@
 #include <QString>
 #include <QElapsedTimer>
 #include <QPointF>
-#inc
+#include <math.h>
+#include <auxutils.h>
+#include <iostream>
+#include <vector>
+
+TensorFlow::TensorFlow(QObject *parent) : QObject(parent)
+{
+    initialized         = false;
+    accelaration        = false;
+    verbose             = true;
+    numThreads          = 1;
+    threshold           = 0.1;
+    has_detection_masks = false;
+    TPU                 = true;
+}
+
+TensorFlow::~TensorFlow(){}
+
+template<class T>
+bool formatImageQt(T* out, QImage image, int image_channels, int wanted_height, int wanted_width, int wanted_channels, bool input_floating, bool scale = false)
+{
+    const float input_mean = 127.5f;
+    const float input_std  = 127.5f;
+
+    // Check same number of channels
+    if (image_channels != wanted_channels)
+    {
+        qDebug() << "ERROR: the image has" << image_channels << " channel
