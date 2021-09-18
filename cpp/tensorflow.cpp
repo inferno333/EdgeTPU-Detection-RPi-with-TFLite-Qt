d2964437a0e86b74ddbc9cc96a3471d62ccc76a6
inferno333 <inferno1968@proton.me>
2021-09-18 21:21:36 +0000

    Bump version to 3.0.3

--- cpp/tensorflow.cpp
+++ cpp/tensorflow.cpp
@@ -289,4 +289,26 @@ bool TensorFlow::initTFLite(int imgHeight, int imgWidth)
     }
 }
 
-// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+// Code from: https://github.com/YijinLiu/tf-cpu/blob/master/benchmark/obj_detect_lite.cc
+// --------------------------------------------------------------------------------------
+template<typename T>
+T* TensorData(TfLiteTensor* tensor, int batch_index);
+
+template<>
+float* TensorData(TfLiteTensor* tensor, int batch_index) {
+    int nelems = 1;
+    for (int i = 1; i < tensor->dims->size; i++) nelems *= tensor->dims->data[i];
+    switch (tensor->type) {
+        case kTfLiteFloat32:
+            return tensor->data.f + nelems * batch_index;
+        default:
+            qDebug() << "Should not reach here!";
+    }
+    return nullptr;
+}
+
+template<>
+uint8_t* TensorData(TfLiteTensor* tensor, int batch_index) {
+    int nelems = 0;
+    for (int i = 1; i < tensor->dims->size; i++)
