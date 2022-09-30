e6ea045396b4b7611d6ecce9e2cc3ce6e8616631
inferno333 <inferno1968@proton.me>
2022-09-30 17:04:17 +0000

    Shorten title

--- cpp/tensorflowthread.cpp
+++ cpp/tensorflowthread.cpp
@@ -9,4 +9,35 @@ void WorkerTF::setTf(TensorFlow *value)
 void WorkerTF::setImgTF(const QImage &value)
 {
     imgTF     = value;
-    videoMod
+    videoMode = false;
+}
+
+void WorkerTF::setVideoInfo(QString s, QString d, bool sAim, bool sInfTime, QMap<QString,bool> aLabels)
+{
+    source = s;
+    destination = d;
+    showAim = sAim;
+    showInfTime = sInfTime;
+    activeLabels = aLabels;
+    videoMode = true;
+}
+
+void WorkerTF::work()
+{   
+    if (!videoMode)
+    {
+        tf->run(imgTF);
+        emit finished();
+        emit results(tf->getKindNetwork(),tf->getResults(),tf->getConfidence(),tf->getBoxes(),tf->getMasks(),tf->getInferenceTime());
+    }
+}
+
+QImage WorkerTF::processImage(QImage img)
+{
+    // Data
+    double        minConf       = tf->getThreshold();
+    int           inferenceTime = tf->getInferenceTime();
+    QStringList   results       = tf->getResults();
+    QList<double> confidence    = tf->getConfidence();
+    QList<QRectF> boxes         = tf->getBoxes();
+    QList<QImag
